2c57f4b79a9d7dc1b695f9496e9d73c5fd787d4f
Lim Swee Tat <st_lim@stlim.net>
2017-08-02 03:37:04 +0800

    Not starting X server

--- usr/local/bin/nvidia-overclock.sh
+++ usr/local/bin/nvidia-overclock.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-PWR=170
+PWR=100
 CLOCK=275
 MEM=1775
 CMD='/usr/bin/nvidia-settings'
@@ -11,8 +11,8 @@ fi
 
 start() {
 	# exec 1> >(/usr/bin/logger -p mail.info -s -t $(basename $0)) 2>&1
-	X :0 -core &
-	sleep 10
+	#X :0 -core &
+	#sleep 10
 	export DISPLAY=:0.0
 	# nvidia-smi -ac 1911,4004
 
@@ -21,19 +21,19 @@ start() {
 	echo "performance" >/sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
 	echo "performance" >/sys/devices/system/cpu/cpu1/cpufreq/scaling_governor
 	echo 2800000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq
-	echo 2800000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq
+	echo 2800000 > /sys/devices/system/cpu/cpu1/cpufreq/scaling_min_freq
 
-	for i in {0..3}; do
+	for i in {0..2}; do
 		nvidia-smi -i ${i} -pm 0
 		nvidia-smi -i ${i} -pl ${PWR}
-		nvidia-smi -i ${i} -ac 4004,1911
-		${CMD} -a [gpu:${i}]/GPUPowerMizerMode=1
-		${CMD} -a [gpu:${i}]/GPUFanControlState=1
-		${CMD} -a [fan:${i}]/GPUTargetFanSpeed=80
-		for x in {3..3}; do
-			${CMD} -a [gpu:${i}]/GPUGraphicsClockOffset[${x}]=${CLOCK}
-			${CMD} -a [gpu:${i}]/GPUMemoryTransferRateOffset[${x}]=${MEM}
-		done
+		#nvidia-smi -i ${i} -ac 4004,1911
+		#${CMD} -a [gpu:${i}]/GPUPowerMizerMode=1
+		#${CMD} -a [gpu:${i}]/GPUFanControlState=1
+		#${CMD} -a [fan:${i}]/GPUTargetFanSpeed=80
+		#for x in {3..3}; do
+		#	${CMD} -a [gpu:${i}]/GPUGraphicsClockOffset[${x}]=${CLOCK}
+		#	${CMD} -a [gpu:${i}]/GPUMemoryTransferRateOffset[${x}]=${MEM}
+		#done
 	done
 #	sleep 10
 #	killall -9 X
