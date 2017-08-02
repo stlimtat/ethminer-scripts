34cd07217bf547509c9339198f86390d0cc38fd2
Lim Swee Tat <st_lim@stlim.net>
2017-08-03 01:26:01 +0800

    Adding the usr/local/cuda and opencl farm files

--- usr/local/bin/cuda-farm.sh
+++ usr/local/bin/cuda-farm.sh
@@ -2,22 +2,21 @@
 
 GETH=/usr/bin/geth
 ETHMINER=/usr/local/bin/ethminer
-MYCMD=${0}
-IDENTITY=$( basename ${MYCMD} | sed -e 's/-farm.sh//' )
+IDENTITY=$( basename $0 | sed -e 's/-farm.sh//' )
 
 export GPU_FORCE_64BIT_PTR=0
 export GPU_MAX_HEAP_SIZE=100
 export GPU_USE_SYNC_OBJECTS=1
 export GPU_MAX_ALLOC_PERCENT=100
 export GPU_SINGLE_ALLOC_PERCENT=100
-exec 1> >(/usr/bin/logger -p mail.info -s -t $(basename $0)) 2>&1
+exec 1> >(/usr/bin/logger -p local0.info -s -t ${IDENTITY}) 2>&1
 
 start() {
 	lshw -c video | grep "bus info:"
 	$ETHMINER --list-devices
 	sudo -s -u st_lim \
-		$ETHMINER --farm-recheck 200 \
-		--verbosity 7 \
+		$ETHMINER --farm-recheck 2000 \
+		--verbosity 1 \
 		-S asia1.ethermine.org:4444 \
 		-FS asia1.ethermine.org:14444 \
 		--stratum-email st_lim\@stlim.net \
@@ -31,7 +30,11 @@ stop() {
 	FARM_PID=$(/bin/ps -eww f | grep ${IDENTITY}-farm | grep start | awk '{ print $1 }')
 	kill -9 $ETHMINER_PID
 	kill -9 $FARM_PID
-	sleep 10
+	ETHMINER_PID=$(/bin/ps -eww f | grep ethminer | grep ${IDENTITY} | awk '{ print $1 }')
+	while [ -n "$ETHMINER_PID" ]; do
+		sleep 2
+		ETHMINER_PID=$(/bin/ps -eww f | grep ethminer | grep ${IDENTITY} | awk '{ print $1 }')
+	done
 }
 
 reload() {
