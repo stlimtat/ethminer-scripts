a0ae6466d669af345a5506e2e90083c4f842cadf
Lim Swee Tat <st_lim@stlim.net>
2017-08-03 02:36:58 +0800

    Finished debugging the monitorFarm script and ready for primetime

--- usr/local/bin/monitorFarm.sh
+++ usr/local/bin/monitorFarm.sh
@@ -1,46 +1,44 @@
-#!/bin/bash -x
+#!/bin/bash
 
-ETHMINER_ARCH=$1
+PARAM_1=$1
 
-if [ -z "$ETHMINER_ARCH" ]; then
+if [ -z "$PARAM_1" ]; then
 	echo "No parameters found.  Please provide either 0 or 1"
 	exit 1
 fi
 
-LOCK=/tmp/$(basename $0)-${ETHMINER_ARCH}.lock
+LOCK=/tmp/$(basename $0)-${PARAM_1}.lock
 if [ -f ${LOCK} ]; then
 	exit 0
 fi
 
 touch ${LOCK}
 LOG_HOME=/home/st_lim/local
-ETHMINER_LOG_ARRAY[0]=${LOG_HOME}0.log
-ETHMINER_LOG_ARRAY[1]=${LOG_HOME}1.log
-ETHMINER_INSTANCE_ARRAY[0]=cuda
-ETHMINER_INSTANCE_ARRAY[1]=opencl
+IDENTITY_ARRAY[0]=cuda
+IDENTITY_ARRAY[1]=opencl
 
-ETHMINER_LOG=${ETHMINER_LOG_ARRAY[$ETHMINER_ARCH]}
-ETHMINER_INSTANCE=${ETHMINER_INSTANCE_ARRAY[$ETHMINER_ARCH]}
-ETHMINER_LOG=${LOG_HOME}test.log
+ETHMINER_LOG=${LOG_HOME}${PARAM_1}.log
+IDENTITY=${IDENTITY_ARRAY[$PARAM_1]}
+# ETHMINER_LOG=${LOG_HOME}test.log
 
-ETHMINER_ERRORS[0]="Read response failed: End of file"
+ETHMINER_ERRORS[0]="Read response failed. End of file"
 ETHMINER_ERRORS[1]="Could not resolve"
-ETHMINER_ERRORS[2]="submit solution: Not connected"
+ETHMINER_ERRORS[2]="submit solution. Not connected"
+
+exec 1> >(/usr/bin/logger -p local2.info -s -t ${IDENTITY}) 2>&1
 
 if [ $( dmesg | grep -c 'GPU has fallen off') -ne 0 ]; then
-	sudo systemctl reboot --force --no-wall
+	systemctl reboot --force --no-wall
 else
 	for MYERR_COUNT in {0..2}; do
 		MYERR=${ETHMINER_ERRORS[$MYERR_COUNT]}
-		FOUND=$(tail -n5 $ETHMINER_LOG | grep "$MYERR" | wc -l)
-		if [ -n ${FOUND} ]; then
-			echo "Found (${FOUND}) instance of error message (${MYERR}) in (${ETHMINER_LOG}) - Killing (${ETHMINER_INSTANCE})"
-			while [ -n ${FOUND} ]; do
-				systemctl restart ${ETHMINER_INSTANCE}-farm
-				sleep 10
-				FOUND=$(tail -n5 $ETHMINER_LOG | grep "$MYERR" | wc -l)
-			done
-		fi
+		FOUND=$(tail -n5 $ETHMINER_LOG | grep --count "$MYERR")
+		while [ ${FOUND} -gt 0 ]; do
+			echo "Found (${FOUND}) instance of error message (${MYERR}) in (${ETHMINER_LOG}) - Killing (${IDENTITY})"
+			systemctl restart ${IDENTITY}-farm
+			sleep 2
+			FOUND=$(tail -n5 $ETHMINER_LOG | grep --count "$MYERR")
+		done
 	done
 fi
 rm ${LOCK}
